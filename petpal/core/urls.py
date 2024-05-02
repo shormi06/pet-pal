d9387c15e5429e2b87a04585be6be42355a5e870
tousifnirob1 <tnirob40@gmail.com>
2024-05-02 12:25:27 +0600

    core app added

---
+++ petpal/core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index.html/', views.index, name='index'),
+    path('index.html/topics-detail.html/', views.index, name='index'),
+    path('topics-detail.html/', views.topics_detail, name='topics-detail'),
+    path('signup/', views.SignupPage, name='signup'),
+    path('login/', views.LoginPage, name='login'),
+    path('add_pet/', views.add_pet, name='add_pet'),
+    path('add_caregiver/', views.add_caregiver, name='add_caregiver'),
+]
